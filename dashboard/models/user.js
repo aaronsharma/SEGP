ab25ceca3f21c9aaaddc3d94c45bb8ee251da382
fenr97 <fennerav@googlemail.com>
2021-04-24 19:29:31 +0100

    changed user model

--- dashboard/models/user.js
+++ dashboard/models/user.js
@@ -10,7 +10,15 @@ var userSchema = new Schema(
       type: Number,
       min: [0, 'Too little power usage'],
       max: [10000, 'Too much power usage']
-    }
+    },
+    device0: {type: number, max: 100, min:0},
+    device1: {type: number, max: 100, min:0},
+    device2: {type: number, max: 100, min:0},
+    device3: {type: number, max: 100, min:0},
+    device4: {type: number, max: 100, min:0},
+    device5: {type: number, max: 100, min:0},
+    device6: {type: number, max: 100, min:0},
+    device7: {type: number, max: 100, min:0}
   }
 );
 
--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {InputdataComponent} from './view/inputdata/inputdata.component';
 import {BarChartComponent} from './bar-chart/bar-chart.component';
-import {InputdataComponent} from './view/inputdata/inputdata.component'
 
-//is this where i put the route to the homepage
+
 const routes: Routes = [
   {path:"getdata/:name", component:InputdataComponent},
-  {path: 'bar-chart', component: BarChartComponent},
-  {path: '**', component: BarChartComponent}
+  // {path: 'bar-chart', component: BarChartComponent},
+  // {path: '**', component: BarChartComponent}
 ];
 
 @NgModule({
