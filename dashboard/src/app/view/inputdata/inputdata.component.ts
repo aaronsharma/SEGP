226079bbddd386fd26ae0a7b8eb98ac1b83281ab
ShiruoH <2016301200318@whu.edu.cn>
2021-04-30 19:42:01 +0100

    btn changged

--- dashboard/src/app/view/inputdata/inputdata.component.ts
+++ dashboard/src/app/view/inputdata/inputdata.component.ts
@@ -10,14 +10,15 @@ import { HttpClient, HttpParams } from "@angular/common/http"
 })
 export class InputdataComponent implements OnInit {
   public user:any;
-  public getchart:boolean=false;
   constructor(private router: ActivatedRoute,private http:HttpClient) {
     console.log(this);
     console.log(this.router.params);
    }
   public barchart:any;
   Device = "";
+  deviceCount:number=0;
   Hours:number=0;
+
   hours0:number=0;
   hours1:number=0;
   hours2:number=0;
@@ -28,56 +29,44 @@ export class InputdataComponent implements OnInit {
   hours7:number=0;
 
 
-  clickGetDvc(d:number){
-    switch (d) {
-      case 0:this.Device='';break;
-      case 1:this.Device='Washing machine';break;
-      case 2:this.Device='Kettle';break;
-      case 3:this.Device='Television(lcd)';break;
-      case 4:this.Device='Game Console';break;
-      case 5:this.Device='Tablet/hand-held game';break;
-      case 6:this.Device='Mobile phone';break;
-      case 7:this.Device='Laptop PC';break;
-      case 8:this.Device='Desktop PC';break;
+  clickGetDvc(){
+    switch (this.deviceCount) {
+      case 0:this.Device='Washing machine';break;
+      case 1:this.Device='Kettle';break;
+      case 2:this.Device='Television(lcd)';break;
+      case 3:this.Device='Game Console';break;
+      case 4:this.Device='Tablet/hand-held game';break;
+      case 5:this.Device='Mobile phone';break;
+      case 6:this.Device='Laptop PC';break;
+      case 7:this.Device='Desktop PC';break;
       default:
         break;
     }
   }
-  //tried to get data from webpage but failed
-   /*async getData(){
-   let httpUrl = 'http://localhost:4200//getdata/chart'
-    let result = await axios.get(httpUrl);
-    console.log(result.da)
-  }*/
-  //inputdata.component.html line 31 the button 'show' uses this function
-  clickLinkDB(){
-    //only if there are two inputs: device and hours will the server work and show barchart
-    if((this.Hours>0)&&(this.Device!="")){
-      this.getdatafromDB(this.Device,this.Hours);
-      //in inputdata.component.html line 34, only if the 'getchar==true' will the picture shows. 
-      //and I added a pic(not barchart) to see if it functions well, after we get it connected to DB
-      //we could replace it with our barchart 
-      this.getchart=true;
-    }
-  }
-  //unfinished, need to connect with database and get data
-  getdatafromDB(device:string, hrs:number){
-    
+
+  back(){
+    if(this.deviceCount>0)
+      this.deviceCount--;
+    this.clickGetDvc();
+    this.Hours=0;
   }
 
-  setDeviceUsage(device:string, hrs:number){
-    switch(device){
-      case 'Washing machine': this.hours0 = hrs;break;
-      case 'Kettle': this.hours1 = hrs;break;
-      case 'Television(lcd)': this.hours2 = hrs;break;
-      case 'Game Console': this.hours3 = hrs;break;
-      case 'Tablet/hand-held game': this.hours4 = hrs;break;
-      case 'Mobile phone': this.hours5 = hrs;break;
-      case 'Laptop PC': this.hours6 = hrs;break;
-      case 'Desktop PC': this.hours7 = hrs;break;
+  setDeviceUsage(){
+    this.clickGetDvc();
+    switch(this.Device){
+      case 'Washing machine': this.hours0 = this.Hours;break;
+      case 'Kettle': this.hours1 = this.Hours;break;
+      case 'Television(lcd)': this.hours2 = this.Hours;break;
+      case 'Game Console': this.hours3 = this.Hours;break;
+      case 'Tablet/hand-held game': this.hours4 = this.Hours;break;
+      case 'Mobile phone': this.hours5 = this.Hours;break;
+      case 'Laptop PC': this.hours6 = this.Hours;break;
+      case 'Desktop PC': this.hours7 = this.Hours;break;
       default: break;
     }
-    this.getchart=true;
+    if(this.deviceCount<8)
+      this.deviceCount++;
+    this.Hours=0;
   }
 
   calcTotalUsage(){
@@ -88,6 +77,7 @@ export class InputdataComponent implements OnInit {
 
   //get user's name thought the link
   ngOnInit(): void {
+    this.clickGetDvc();
     this.router.params.subscribe((params)=>{
       if(params.name=='visitor')
         this.user="";
